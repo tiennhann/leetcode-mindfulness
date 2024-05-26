54679acf8e286d8d8fc7d590470f705b8c94557d
Long Tran <longdtranw@gmail.com>
2024-05-26 16:26:40 -0600

    found the name of the problem solved

--- chrome-extension/scripts/content.js
+++ chrome-extension/scripts/content.js
@@ -1,34 +1,58 @@
-function parseProblems() {
-  let nextButton = document.querySelector('button[aria-label="next"]:enabled')
-  nextButton.click();  
-  // let buttons = Array.from(document.querySelectorAll('button'));
-  // buttons = buttons.filter(button => button.innerText == "Status");
-  // const statusButton = buttons[0];
-  // statusButton.click();
-  // let options = Array.from(document.querySelectorAll('div[role="menuitem"]:has(span)'));
-  // options = options.filter(option => option.innerText == "Solved")
-  // console.log(options)
-  // let option = options[0];
-  // option.click()
-
-  // let  rowgroups = Array.from(document.querySelectorAll('div[role="rowgroup"]'));
-  // rowgroups = rowgroups.filter(rowgroup => rowgroup.querySelector("div[role='row']") != null)
-  // let rowgroup = rowgroups[0]
-  // let rows = Array.from(rowgroup.querySelectorAll('div[role="row"]'));
-  // rows = rows.map(row => row.querySelector('span[data-state="closed"]'))
-  // console.log(Array.from(rows));
+async function getSolved(limit=50) {
+  async function getProblems(skip) {
+    // do something with response here, not outside the function
+    const response = await fetch("https://leetcode.com/graphql", {
+      method: "POST",
+      body: JSON.stringify({
+        "query": "\n    query problemsetQuestionList($categorySlug: String, $limit: Int, $skip: Int, $filters: QuestionListFilterInput) {\n  problemsetQuestionList: questionList(\n    categorySlug: $categorySlug\n    limit: $limit\n    skip: $skip\n    filters: $filters\n  ) {\n    total: totalNum\n    questions: data {\n      acRate\n      difficulty\n      freqBar\n      frontendQuestionId: questionFrontendId\n      isFavor\n      paidOnly: isPaidOnly\n      status\n      title\n      titleSlug\n      topicTags {\n        name\n        id\n        slug\n      }\n      hasSolution\n      hasVideoSolution\n    }\n  }\n}\n    ",
+        "variables": {
+          "categorySlug": "all-code-essentials",
+          "skip": skip,
+          "limit": limit,
+          "filters": {
+            "status": "AC"
+          }
+        },
+        "operationName": "problemsetQuestionList"
+      }),
+      headers: {
+        "Content-type": "application/json"
+      }
+    });
+    const data = await response.json();
+    const problemsList = data["data"]['problemsetQuestionList'];
+    return problemsList;
+  } // end getProblems
+  
+  // get the first limit
+  let problemsList = await getProblems(0);
+  if (problemsList === undefined) {
+    console.error("failed to get problem list");
+    return;
   }
-
+  const size = problemsList.total;
+  let problems = problemsList['questions'];
+  // check if there is more
+  if (size > problems.length) {
+    // get the rest of the data
+    const times = Math.ceil((size - limit) / limit);
+    for (let i = 0; i < times; ++i) {
+      problemsList = await getProblems((i+1) * limit);
+      problems.push(...problemsList['questions']);
+    } // end for i
+  } // end if
+  return problems;
 } // end parseProblems
 
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    parseProblems();
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting === "hello")
-      sendResponse({farewell: "goodbye"});
+    if (request.reason === "getsolved") {
+      getSolved().then((data) => {
+          console.log(data);
+          sendResponse(data);
+      });
+      return true;
+    } // end if
   }
-);
+);      
--- chrome-extension/scripts/main.js
+++ chrome-extension/scripts/main.js
@@ -1,19 +1,8 @@
-document.querySelector("p").textContent = "Hello from main.js!";
+document.querySelector("p#des").textContent = "Click to see the problems";
 document.querySelector("button").addEventListener("click", () => {
     (async () => {
         const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-        const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
-        // do something with response here, not outside the function
-        console.log(response);
+        const response = await chrome.tabs.sendMessage(tab.id, {reason: "getsolved"});
+        document.querySelector("p#output").textContent = `${response.length} solved problems`;
       })();
 })
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      if (request.greeting === "hello")
-        sendResponse({farewell: "goodbye"});
-    }
-  );
